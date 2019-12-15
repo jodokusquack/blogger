fd5ba122e09321df5035fc2c6d0e189e57206c64
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-12-15 21:48:07 +0100

    Added paperclip gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
+  has_attached_file :image, styles: {medium:"150x150"}
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   def tag_list
     tags.join(", ")
   end
